a9eb53d39d85c47871fc5b005be7085b68cd407d
IIvexII <mrh4ck3r1738@gmail.com>
2022-09-17 12:58:49 +0500

    Added new Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+
+import Header from './component/Header';
+
+export default function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
