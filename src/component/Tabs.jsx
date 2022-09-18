deb82f60d4f9213568823f238ed71ac316c2421b
IIvexII <mrh4ck3r1738@gmail.com>
2022-09-18 19:28:12 +0500

    Tabs Completed

--- src/component/Tabs.jsx
+++ src/component/Tabs.jsx
@@ -1,26 +1,30 @@
+import { Fragment } from 'react';
 import './Tabs.css';
 export default function Tabs() {
   return (
-    <section className='tabs'>
-      <ul>
-        <li>
-          <a href='#' className='active-tab'>
-            Overview
-          </a>
-        </li>
-        <li>
-          <a href='#'>Repositories</a>
-        </li>
-        <li>
-          <a href='#'>Projects</a>
-        </li>
-        <li>
-          <a href='#'>Packages</a>
-        </li>
-        <li>
-          <a href='#'>Stars</a>
-        </li>
-      </ul>
-    </section>
+    <Fragment>
+      <section className='tabs'>
+        <ul>
+          <li>
+            <a href='#' className='active-tab'>
+              Overview
+            </a>
+          </li>
+          <li>
+            <a href='#'>Repositories</a>
+          </li>
+          <li>
+            <a href='#'>Projects</a>
+          </li>
+          <li>
+            <a href='#'>Packages</a>
+          </li>
+          <li>
+            <a href='#'>Stars</a>
+          </li>
+        </ul>
+      </section>
+      <div className='divider'></div>
+    </Fragment>
   );
 }
