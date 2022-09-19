83000b8f074e6a4287b2bb17749e99c16cf8650e
IIvexII <mrh4ck3r1738@gmail.com>
2022-09-19 20:52:39 +0500

    Added new section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Fragment } from 'react';
 import Header from './component/Header';
 import './App.css';
 import Tabs from './component/Tabs';
+import UserInfo from './component/UserInfo';
 
 export default function App() {
   return (
@@ -10,13 +11,7 @@ export default function App() {
       <Header />
       <Tabs />
       <main>
-        <section className='user-info'>
-          <img
-            src={require('./images/avatar.jpg')}
-            alt='My avatar with yellow background and headphones on the ear.'
-            className='user-avatar'
-          />
-        </section>
+        <UserInfo />
       </main>
     </Fragment>
   );
---
+++ src/component/UserInfo.jsx
@@ -0,0 +1,16 @@
+import './UserInfo.css';
+export default function UserInfo() {
+  return (
+    <section className='user-info'>
+      <img
+        src={require('../images/avatar.jpg')}
+        alt='My avatar with yellow background and headphones on the ear.'
+        className='user-avatar'
+      />
+      <article>
+        <h2 className='name'>Zafeer Hafeez</h2>
+        <h2 className='username'>IIvexII</h2>
+      </article>
+    </section>
+  );
+}
