269a73ce103d1d55a5d10fcb3aca186156d96878
IIvexII <mrh4ck3r1738@gmail.com>
2022-09-21 21:06:06 +0500

    Added address

--- src/component/UserInfo.jsx
+++ src/component/UserInfo.jsx
@@ -14,16 +14,23 @@ export default function UserInfo() {
         <h2 className='name'>Zafeer Hafeez</h2>
         <h2 className='username'>IIvexII</h2>
         <p className='bio'>Student of Computer Science.</p>
+        {/* Edit button */}
         <button type='button'>Edit profile</button>
+        {/* Followers */}
         <div id='follow'>
           <a href='/'>
-            <UserIcon className='user-icon' />
+            <UserIcon className='icon' />
             <span className='bold'>6</span> followers
           </a>
           <a href='/'>
             <span className='bold'>2</span> following
           </a>
         </div>
+        {/* Addesss and social media links */}
+        <address>
+          <LocationIcon className='icon' />
+          127.0.0.1
+        </address>
       </article>
     </section>
   );
