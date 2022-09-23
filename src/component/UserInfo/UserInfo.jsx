cedd356164f47bc1f2d51094cb7bb6f46f8765e6
IIvexII <mrh4ck3r1738@gmail.com>
2022-09-23 12:02:46 +0500

    Organization logic seperated.

--- src/component/UserInfo/UserInfo.jsx
+++ src/component/UserInfo/UserInfo.jsx
@@ -5,6 +5,7 @@ import DeveloperProgram from './DeveloperProgram';
 
 // Icons
 import { UserIcon, EmailIcon, LinkIcon, StarIcon, LocationIcon } from './icons';
+import Organizations from './Organizations';
 
 export default function UserInfo(props) {
   const user = props.user;
@@ -26,26 +27,6 @@ export default function UserInfo(props) {
     );
   }
 
-  // Render Orgs
-  let renderOrgs = null;
-  if (user.orgs && user.orgs.length !== 0) {
-    renderOrgs = (
-      <div className='orgs'>
-        <h3 className='title'>Organizations</h3>
-        {user.orgs.map((org) => {
-          return (
-            <a href='/' key={org._id}>
-              <img
-                className='org-img mtb-20 mr-10'
-                src={require(`../../images/${org.icon}`)}
-                alt={user.orgs.description}
-              />
-            </a>
-          );
-        })}
-      </div>
-    );
-  }
   return (
     <section className='user-info'>
       <img
@@ -102,7 +83,7 @@ export default function UserInfo(props) {
         </div>
         <div className='divider mtb-20'></div>
         {/* Organizations */}
-        {renderOrgs}
+        <Organizations orgs={user.orgs} />
       </article>
     </section>
   );
