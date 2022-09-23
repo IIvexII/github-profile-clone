8f5ce1dbd3ae6c3d3a4d2a24a2e18c6602b8ff39
IIvexII <mrh4ck3r1738@gmail.com>
2022-09-23 10:55:05 +0500

    completed the user info component

--- src/component/UserInfo.jsx
+++ src/component/UserInfo.jsx
@@ -5,6 +5,8 @@ import { ReactComponent as LocationIcon } from '../images/location.svg';
 import { ReactComponent as UserIcon } from '../images/userIcon.svg';
 import { ReactComponent as EmailIcon } from '../images/email.svg';
 import { ReactComponent as LinkIcon } from '../images/link.svg';
+import { ReactComponent as DevProgramIcon } from '../images/dev-program.svg';
+import { ReactComponent as StarIcon } from '../images/star.svg';
 
 export default function UserInfo() {
   return (
@@ -46,6 +48,39 @@ export default function UserInfo() {
           <a className='link pt-10' href='https://iivexii.me'>
             <LinkIcon className='icon' />
             https://iivexii.me
+          </a>
+        </div>
+        <div className='divider mtb-20'></div>
+        {/* Showcase */}
+        <div className='showcase'>
+          <a className='link pt-10' href='/'>
+            <DevProgramIcon className='icon' />
+            Developer Program Member
+          </a>
+          {/* Labels */}
+          <span className='labels pt-10'>
+            <StarIcon />
+            <p className='label premium mlr-10'>pro</p>
+            <p className='label gold'>Developer</p>
+          </span>
+        </div>
+        <div className='divider mtb-20'></div>
+        {/* Organizations */}
+        <div className='orgs'>
+          <h3 className='title'>Organizations</h3>
+          <a href='/'>
+            <img
+              className='org-img mtb-20 mr-10'
+              src={require('../images/org-img.jpg')}
+              alt='Vex organization.'
+            />
+          </a>
+          <a href='/'>
+            <img
+              className='org-img mtb-20 mr-10'
+              src={require('../images/npm-org.png')}
+              alt='NPM organization.'
+            />
           </a>
         </div>
       </article>
