f98048c3caaf462d3d6f23f05c0e5eae0126287c
IIvexII <mrh4ck3r1738@gmail.com>
2022-09-25 14:02:16 +0500

    All the components are moved to their own folders with css and sub-components

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -1,4 +1,4 @@
-import { ReactComponent as Logo } from '../images/logo.svg';
+import { ReactComponent as Logo } from '../../images/logo.svg';
 import './Header.css';
 
 export default function Header() {
@@ -24,16 +24,16 @@ export default function Header() {
         <nav className='nav-bar'>
           <ul>
             <li>
-              <a href='.'>Pull requests</a>
+              <a href='/'>Pull requests</a>
             </li>
             <li>
-              <a href='.'>Issues</a>
+              <a href='/'>Issues</a>
             </li>
             <li>
-              <a href='.'>Marketplace</a>
+              <a href='/'>Marketplace</a>
             </li>
             <li>
-              <a href='.'>Explore</a>
+              <a href='/'>Explore</a>
             </li>
           </ul>
         </nav>
