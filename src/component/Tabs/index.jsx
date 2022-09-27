d27008e917c2bf5ce8b876ae8e7a6aa346a7ea3f
IIvexII <mrh4ck3r1738@gmail.com>
2022-09-27 16:36:39 +0500

    Fixed the issues with tabs

--- src/component/Tabs/index.jsx
+++ src/component/Tabs/index.jsx
@@ -4,25 +4,27 @@ export default function Tabs() {
   return (
     <Fragment>
       <section className='tabs'>
+        <div></div>
         <ul>
           <li>
-            <a href='#' className='active-tab'>
+            <a href='/' className='active-tab'>
               Overview
             </a>
           </li>
           <li>
-            <a href='#'>Repositories</a>
+            <a href='/'>Repositories</a>
           </li>
           <li>
-            <a href='#'>Projects</a>
+            <a href='/'>Projects</a>
           </li>
           <li>
-            <a href='#'>Packages</a>
+            <a href='/'>Packages</a>
           </li>
           <li>
-            <a href='#'>Stars</a>
+            <a href='/'>Stars</a>
           </li>
         </ul>
+        <div></div>
       </section>
       <div className='divider'></div>
     </Fragment>
